cf3e7e971effe720204ed0665b6b1f5a868342ba
Ethan <ethan.scott2@icloud.com>
2023-11-28 09:46:23 -0600

    Initial Commit

---
+++ Running app/Running_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Running_appApp.swift
+//  Running app
+//
+//  Created by Ethan Scott on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Running_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
