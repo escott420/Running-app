8e9afd076852ab826aa2de7476306a0eed523fe3
Ethan <ethan.scott2@icloud.com>
2023-11-28 09:58:04 -0600

    Added text fields for user to enter time and distance ran

--- Running app/ContentView.swift
+++ Running app/ContentView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enterTimeTextField = ""
+    @State private var enterDistanceTextField = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Running Calculator!")
+            HStack{
+                CustomTextField(placeholder: "Enter time ran", variable: $enterTimeTextField)
+                CustomTextField(placeholder: "Enter miles ran", variable: $enterDistanceTextField)
+            }
         }
+        
         .padding()
     }
 }
@@ -24,3 +31,24 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+struct CustomTextField: View {
+    let placeholder : String
+    let variable : Binding<String>
+    var body: some View {
+        TextField(placeholder, text: variable)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .multilineTextAlignment(.center)
+            .frame(width: 160, height: 35, alignment: .center)
+            .font(.body)
+            .padding()
+    }
+}
+struct CustomText: View {
+    let text : String
+    var body: some View {
+        Text(text)
+            .font(.title)
+            .fontWeight(.bold)
+    }
+}
+
